d8d88f3f4625f0e3136e1137dce804e851176290
gabii.ak <elpollokawaiicomecoco@gmail.com>
2026-08-12 21:50:08 -0300

    estilo windows xp

--- wishlist/wishlist.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("cucaracha").addEventListener("click", function(){
-    const respuesta = confirm("te gustaría donar todo tu dinero al PUEBLO DE ISRAEL?")
-    if(respuesta){window.location.href = "israel.html"}
-});
-document.getElementById("tux").addEventListener("click", function(){window.location.href="../index.html"});
